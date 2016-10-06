e8341ca1c35effb6c85519be7efd477767d81c45
meilinlu86@gmail.com <ml473@afsconnect1.njit.edu>
2016-10-06 06:48:59 -0400

    3 html pages

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+  echo 'hello world';
+  $myvar = 'Some Text I want to store in a variable';
+  echo '<br>'
+  echo '$myvar'
+  echo '<br>'
+  echo "$myvar";
+  echo '<br>';
+
+  $url = 'http://www.google.com';
+  $linkname = 'Google';
+  echo 'a href="'.$url.'">'.$linkname.'</a>';
+
+
+
+
+?>
